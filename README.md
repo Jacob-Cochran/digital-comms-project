bce1ed10b7f669a8ad52ef05059f4e49feeb1851
jcochran <jcochran@spectric.com>
2025-12-08 16:27:31 -0500

    cleaned up all the old stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-This is managed with conda because GNU radio isn't super stable on centos,
-activate the environment with
-
-conda activate gnuradio
--- picamera_test2.py
+++
@@ -1,9 +0,0 @@
-from picamera2 import Picamera2
-
-picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)}))
-picam2.start()
-
-frame = picam2.capture_array()  # returns a numpy array
-
-print(frame)
